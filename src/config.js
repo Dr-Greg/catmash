d2bb111ca9daadc605d0bf4b58108bc26696162d
Dr-Greg <grigor.mouradyan@outlook.fr>
2020-10-29 10:23:02 +0100

    chore: server config files

---
+++ src/config.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+
+const CONFIG = {
+  PORT: process.env.PORT || '3000',
+  HOST: process.env.HOST || '127.0.0.1',
+  DB_CONNECTION_STRING: process.env.DB_CONNECTION_STRING || 'mongodb://localhost:27017',
+  DB_NAME: process.env.DB_NAME || 'default',
+  ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET,
+  DEV: process.env.ENVIRONMENT === 'dev',
+};
+
+module.exports = CONFIG;
