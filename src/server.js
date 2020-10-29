90ac3c031e294ae4987098dba1d7fa31c31fcff7
Dr-Greg <grigor.mouradyan@outlook.fr>
2020-10-29 10:22:31 +0100

    feat: implementing basic server

---
+++ src/server.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+const CONFIG = require('./config');
+const app = require('./app');
+const { db } = require('./middlewares');
+
+db.connect();
+
+http
+  .createServer(app)
+  .on('error', (err) => {
+    console.error({ err }, 'The HTTP server threw an error. Exiting.');
+    process.exit(1);
+  })
+  .listen(CONFIG.PORT, CONFIG.HOST, () =>
+    console.log(`Server running at http://${CONFIG.HOST}:${CONFIG.PORT}`)
+  );
