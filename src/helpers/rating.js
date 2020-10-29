fd738b626c76548d7d2c5f91c9b761e13c43f65b
Dr-Greg <grigor.mouradyan@outlook.fr>
2020-10-29 10:17:12 +0100

    feat: elo rating algorithm

---
+++ src/helpers/rating.js
@@ -0,0 +1,16 @@
+const Probability = (rateX, rateY) => 1 / (1 + 10 ** ((rateX - rateY) / 400));
+
+module.exports = (rateA, rateB, K, d) => {
+  const probA = Probability(rateB, rateA);
+  const probB = Probability(rateA, rateB);
+
+  if (d)
+    return {
+      rateA: Math.round(rateA + K * (1 - probA)),
+      rateB: Math.round(rateB + K * (0 - probB)),
+    };
+  return {
+    rateA: Math.round(rateA + K * (0 - probA)),
+    rateB: Math.round(rateB + K * (1 - probB)),
+  };
+};
